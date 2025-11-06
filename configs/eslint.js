b0f0f952e524d4c7142fcb805ee687ffa8915fa0
Ben Wells <ben@bigbite.net>
2025-11-06 20:32:47 +0000

    feat: add eslint and prettier configs

---
+++ configs/eslint.js
@@ -0,0 +1,52 @@
+const prettierConfig = require("./prettier.js");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:@wordpress/eslint-plugin/recommended-with-formatting", // no prettier, using the version with prettier in the plugin will cause a conflict
+    "plugin:prettier/recommended", // uses our prettier config
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    "jsdoc/require-returns-description": 0, // we don't always need a description
+    "@wordpress/i18n-text-domain": 0, // we prefer to always set the text domain, including when using 'default'
+    "react-hooks/exhaustive-deps": "error", // increase wp-scripts rule to from warn to error
+    complexity: ["error", 10],
+    "prettier/prettier": [
+      "error",
+      prettierConfig,
+      {
+        usePrettierrc: false,
+      },
+    ],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+        peerDependencies: true,
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+        },
+        warnOnUnassignedImports: true,
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@wordpress/**",
+            group: "builtin",
+            position: "before",
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ configs/prettier.js
@@ -0,0 +1,10 @@
+const config = {
+  tabs: false,
+  printWidth: 100,
+  trailingComma: "all",
+  arrowParens: "always",
+  singleQuote: true,
+  endOfLine: "auto",
+};
+
+module.exports = config;
